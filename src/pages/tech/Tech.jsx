2a5a912214d57c897fdb4c6bfa5d9a0222ffc18f
tmubei <326138915@qq.com>
2019-12-24 01:55:50 +0800

    add threejs

--- src/pages/tech/Tech.jsx
+++ src/pages/tech/Tech.jsx
@@ -1,10 +1,59 @@
 import React from 'react';
+import { Link, Route,Switch,Redirect } from "react-router-dom";
+import { Row, Col } from "antd";
 import './Tech.less';
+
+import Test from './test/Test';
+import Test1 from './test/Test1';
 
 function  Tech(props){
     return (
         <div className="tech">
-            <h1>敬请期待</h1>
+            <Row>
+                <Col span={4}>
+                    <div style={{borderRight:"2px solid #f2f4f8",height:"500px"}}>
+                        <h1>导航</h1>
+                        <ul>
+                            <li><Link to="/tech/test1">test</Link></li>
+                            {/* <li><Link to="/tech/test2">阿斯顿发</Link></li>
+                            <li><Link to="/tech/test3">阿斯顿发</Link></li>
+                            <li><Link to="/tech/test4">阿斯顿发</Link></li>
+                            <li><Link to="/tech/test5">阿斯顿发</Link></li>
+                            <li><Link to="/tech/test6">阿斯顿发</Link></li>
+                            <li><Link to="/tech/test7">阿斯顿发</Link></li>
+                            <li><Link to="/tech/test">阿斯顿发</Link></li>
+                            <li><Link to="/tech/test">阿斯顿发</Link></li>
+                            <li><Link to="/tech/test">阿斯顿发</Link></li>
+                            <li><Link to="/tech/test">阿斯顿发</Link></li>
+                            <li><Link to="/tech/test">阿斯顿发</Link></li>
+                            <li><Link to="/tech/test">阿斯顿发</Link></li>
+                            <li><Link to="/tech/test">阿斯顿发</Link></li>
+                            <li><Link to="/tech/test">阿斯顿发</Link></li>
+                            <li><Link to="/tech/test">阿斯顿发</Link></li>
+                            <li><Link to="/tech/test">阿斯顿发</Link></li>
+                            <li><Link to="/tech/test">阿斯顿发</Link></li>
+                            <li><Link to="/tech/test">阿斯顿发</Link></li> */}
+                            <li><Link to="/tech/test">test1</Link></li>
+                        </ul>
+                    </div>
+                </Col>
+                <Col span={20}>
+                    <h1>threejs-test</h1>
+                    <div>
+                        <Switch>
+                            <Route path="/tech/test" component={Test}></Route>
+                            <Route path="/tech/test1" component={Test1}></Route>
+                            {/* <Route path="/tech/test2" component={Test}></Route>
+                            <Route path="/tech/test3" component={Test}></Route>
+                            <Route path="/tech/test4" component={Test}></Route>
+                            <Route path="/tech/test5" component={Test}></Route>
+                            <Route path="/tech/test6" component={Test}></Route>
+                            <Route path="/tech/test7" component={Test}></Route> */}
+                            <Redirect to='/tech/test'/>
+                        </Switch>
+                    </div>
+                </Col>
+            </Row>
         </div>
     );
 }
---
+++ src/pages/tech/test/Test.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import * as THREE from "three";
+
+function Test(){
+    const dh = () => {
+        var scene = new THREE.Scene();
+        var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+
+        var renderer = new THREE.WebGLRenderer();
+        renderer.setSize( window.innerWidth, window.innerHeight );
+        document.getElementById("canvas").appendChild( renderer.domElement );
+
+        var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+        var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+        var cube = new THREE.Mesh( geometry, material );
+        scene.add( cube );
+
+        camera.position.z = 5;
+
+        const animate = () => {
+            requestAnimationFrame( animate );
+
+            cube.rotation.x += 0.01;
+            cube.rotation.y += 0.01;
+
+            renderer.render( scene, camera );
+        };
+
+        animate();
+    }
+    return (
+        <div id="canvas">
+            {console.log(document.getElementById("canvas"))}
+            <div onLoad={()=>dh()}>adsfdasf</div>
+        </div>
+    )
+}
+
+export default Test;
